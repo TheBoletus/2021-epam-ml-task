82eca14ee96f04d8f815fedaea5249653323a825
TheBoletus <that.artem@yandex.ru>
2021-11-12 17:32:01 +0300

    Add code for the HTTP service (#5)
    Add code for the HTTP service

---
+++ src/app_service.py
@@ -0,0 +1,82 @@
+"""
+HTTP service for interacting with model.
+
+Supported requests:
+
+- GET /labels
+    Parameters: -
+    Return: JSON with an array of labels available for the model
+
+- POST /predict
+    Parameters: JSON with the following structure
+        {
+            'description': some text for model to analyze
+        }
+    Return: JSON with the following structure
+        {
+            'description': text from the request,
+            'prediction': predicted label as text
+        }
+"""
+
+import logging as lg
+
+from aiohttp import web
+
+from src.util import MODEL_FOLDER, Model, set_logging
+
+routes = web.RouteTableDef()
+model = Model(
+    f'{MODEL_FOLDER}/classifier',
+    f'{MODEL_FOLDER}/labels.json'
+)
+
+
+@routes.get('/labels')
+async def gel_labels(request):
+    """
+    Handler function for the GET /labels endpoint.
+
+    :param request: aiohttp.web.BaseRequest
+    :return: JSON
+    """
+    data = {
+        'labels': model.labels
+    }
+    return web.json_response(
+        data,
+        status=200,
+        content_type='application/json')
+
+
+@routes.post('/predict')
+async def get_prediction(request):
+    """
+    Handler function for the POST /predict endpoint.
+
+    :param request: aiohttp.web.BaseRequest
+    :return: JSON
+    """
+    if not request.body_exists:
+        raise web.HTTPBadRequest
+    req_data = await request.json()
+    desc = req_data.get('description')
+    data = {
+        'description': desc,
+        'prediction': model.get_prediction(desc)
+    }
+    lg.info(f'Prediction generated: {data}')
+    return web.json_response(
+        data,
+        status=200,
+        content_type='application/json')
+
+
+# Main loop
+
+if __name__ == '__main__':
+    lg.info('Starting server')
+    set_logging()
+    app = web.Application()
+    app.add_routes(routes)
+    web.run_app(app)
--- src/util.py
+++ src/util.py
@@ -1,9 +1,75 @@
+import json
 import logging
+import pickle
 
 DATASET_FOLDER = './dataset'
 MODEL_FOLDER = './model'
 
 
 def set_logging():
+    """
+    Function configures the logging module, setting the level
+    and format of messages.
+
+    :return: None
+    """
     logging.basicConfig(format='%(asctime)s - %(message)s',
                         level=logging.INFO)
+
+
+class Model:
+
+    """
+    Class for handling the classifier model.
+    """
+
+    def __init__(self, model_path, labels_path):
+        self.model = self.load_model(model_path)
+        self.labels = self.load_labels(labels_path)
+
+    @staticmethod
+    def load_labels(labels_path):
+        """
+        Loads the JSON with array of labels that the model was trained with.
+
+        :param labels_path: str
+        :return: JSON
+        """
+        with open(labels_path, 'r') as _l:
+            return json.load(_l)
+
+    @staticmethod
+    def load_model(model_path):
+        """
+        Loads given model of classifier.
+
+        :param model_path: str
+        :return: binary
+        """
+        with open(model_path, 'rb') as _m:
+            return pickle.load(_m)
+
+    def get_prediction(self, description):
+        """
+        Method transfers given description to model, gets generated prediction
+        and returns a text of label that the prediction corresponds to.
+
+        :param description: str
+        :return: str
+        """
+        prep_description = self.preprocess_description(description)
+        result = self.model.predict([prep_description, ])
+        return self.labels[int(result)]
+
+    @staticmethod
+    def preprocess_description(description):
+        """
+        Method pre-processes given description.
+        Right now the only change is filtering out all non-alphabetic
+        sequences.
+
+        :param description: str
+        :return: str
+        """
+        filtered = [word for word in description.split() if word.isalpha()]
+        return ' '.join(filtered)
